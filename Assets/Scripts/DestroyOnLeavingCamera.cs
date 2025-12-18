f3572557d8f0516fd12f2ee8def6749cce85efe1
Lan021433 <l021433o@student.staffs.ac.uk>
2025-12-18 16:53:57 +0000

    changed destroy projectile
    added code to the destroy on leaving camera script and need to look deeper into ways o create a vector between the player and the mouse to spawn the projectiles concurrent to mouse position

--- Assets/Scripts/DestroyOnLeavingCamera.cs
+++ Assets/Scripts/DestroyOnLeavingCamera.cs
@@ -4,6 +4,11 @@ public class DestroyonLeavingCamera : MonoBehaviour
 {
     private void Start()
     {
+        Destroy(gameObject, 2.0f);
+    }
+
+    private void OnBecameInvisible()
+    {
         Destroy(gameObject);
     }
 }
